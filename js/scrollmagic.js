d9fbb6237570d87af6c5d5480e4becf334dee701
SeongKookKIM <ksk8646@naver.com>
2023-01-31 17:52:25 +0900

    kiaf-Renewal

---
+++ js/scrollmagic.js
@@ -0,0 +1,275 @@
+gsap
+  .timeline({
+    scrollTrigger: {
+      trigger: "#header",
+      start: "top top",
+      end: "200% center",
+      markers: false,
+      scrub: 3,
+      pin: true,
+    },
+  })
+  .to(".sculpture1", { y: innerHeight * -5, duration: 3, scale: 3, opacity: 0 })
+  .to(".sculpture2", { y: innerHeight * -5, duration: 3, scale: 3, opacity: 0 })
+  .to(".sculpture3", { y: innerHeight * -5, duration: 3, scale: 3, opacity: 0 })
+  .to(".sculpture4", { y: innerHeight * -5, duration: 3, scale: 3, opacity: 0 })
+  .to(".sculpture5", { y: innerHeight * -5, duration: 3, scale: 3, opacity: 0 })
+  .to(".sculpture6", { y: innerHeight * -5, duration: 3, scale: 3, opacity: 0 })
+  .to(".sculpture7", { y: innerHeight * -5, duration: 3, scale: 3, opacity: 0 })
+  .to(".sculpture8", { y: innerHeight * -5, duration: 3, scale: 3, opacity: 0 })
+  .to(".sculpture9", { y: innerHeight * -5, duration: 3, scale: 3, opacity: 0 })
+  .to(".sculpture10", {
+    y: innerHeight * -5,
+    duration: 3,
+    scale: 3,
+    opacity: 0,
+  })
+  .to(".sculpture11", {
+    y: innerHeight * -5,
+    duration: 3,
+    scale: 3,
+    opacity: 0,
+  })
+  .to(".sculpture12", {
+    y: innerHeight * -5,
+    duration: 3,
+    scale: 3,
+    opacity: 0,
+  })
+  .to(".sculpture13", {
+    y: innerHeight * -5,
+    duration: 3,
+    scale: 3,
+    opacity: 0,
+  })
+  .to(".sculpture14", {
+    y: innerHeight * -5,
+    duration: 3,
+    scale: 3,
+    opacity: 0,
+  })
+  .to(".sculpture15", {
+    y: innerHeight * -5,
+    duration: 3,
+    scale: 3,
+    opacity: 0,
+  })
+  .to(".sculpture16", {
+    y: innerHeight * -5,
+    duration: 3,
+    scale: 3,
+    opacity: 0,
+  })
+  .to(".sculpture17", {
+    y: innerHeight * -5,
+    duration: 3,
+    scale: 3,
+    opacity: 0,
+  })
+  .to(".sculpture18", {
+    y: innerHeight * -5,
+    duration: 3,
+    scale: 3,
+    opacity: 0,
+  })
+  .to(".sculpture19", {
+    y: innerHeight * -5,
+    duration: 3,
+    scale: 3,
+    opacity: 0,
+  })
+  .to(".sculpture20", {
+    y: innerHeight * -5,
+    duration: 3,
+    scale: 3,
+    opacity: 0,
+  })
+  .to(".sculpture21", {
+    y: innerHeight * -5,
+    duration: 3,
+    scale: 3,
+    opacity: 0,
+  })
+  .to(".sculpture22", {
+    y: innerHeight * -5,
+    duration: 3,
+    scale: 3,
+    opacity: 0,
+  })
+  .to(".sculpture23", {
+    y: innerHeight * -5,
+    duration: 3,
+    scale: 3,
+    opacity: 0,
+  })
+  .to(".sculpture24", {
+    y: innerHeight * -5,
+    duration: 3,
+    scale: 3,
+    opacity: 0,
+  })
+  .to(".sculpture25", {
+    y: innerHeight * -5,
+    duration: 3,
+    scale: 3,
+    opacity: 0,
+  })
+  .to(".sculpture26", {
+    y: innerHeight * -5,
+    duration: 3,
+    scale: 3,
+    opacity: 0,
+  })
+  .to(".sculpture27", {
+    y: innerHeight * -5,
+    duration: 3,
+    scale: 3,
+    opacity: 0,
+  })
+  .to(".sculpture28", {
+    y: innerHeight * -5,
+    duration: 3,
+    scale: 3,
+    opacity: 0,
+  })
+  .to(".sculpture29", {
+    y: innerHeight * -5,
+    duration: 3,
+    scale: 3,
+    opacity: 0,
+  })
+  .to(".sculpture30", {
+    y: innerHeight * -5,
+    duration: 3,
+    scale: 3,
+    opacity: 0,
+  })
+  .to(".sculpture31", {
+    y: innerHeight * -5,
+    duration: 3,
+    scale: 3,
+    opacity: 0,
+  })
+  .to(".sculpture32", {
+    y: innerHeight * -5,
+    duration: 3,
+    scale: 3,
+    opacity: 0,
+  })
+  .to(".sculpture33", {
+    y: innerHeight * -5,
+    duration: 3,
+    scale: 3,
+    opacity: 0,
+  })
+  .to(".sculpture34", {
+    y: innerHeight * -5,
+    duration: 3,
+    scale: 3,
+    opacity: 0,
+  })
+  .to(".sculpture35", {
+    y: innerHeight * -5,
+    duration: 3,
+    scale: 3,
+    opacity: 0,
+  })
+  .to(".sculpture36", {
+    y: innerHeight * -5,
+    duration: 3,
+    scale: 3,
+    opacity: 0,
+  })
+  .to(".sculpture37", {
+    y: innerHeight * -5,
+    duration: 3,
+    scale: 3,
+    opacity: 0,
+  })
+  .to(".sculpture38", {
+    y: innerHeight * -5,
+    duration: 3,
+    scale: 3,
+    opacity: 0,
+  })
+  .to(".sculpture39", {
+    y: innerHeight * -5,
+    duration: 3,
+    scale: 3,
+    opacity: 0,
+  })
+  .to(".sculpture40", {
+    y: innerHeight * -5,
+    duration: 3,
+    scale: 3,
+    opacity: 0,
+  })
+  .to(".sculpture41", {
+    y: innerHeight * -5,
+    duration: 3,
+    scale: 3,
+    opacity: 0,
+  })
+  .to(".sculpture42", {
+    y: innerHeight * -5,
+    duration: 3,
+    scale: 3,
+    opacity: 0,
+  })
+  .to(".sculpture43", {
+    y: innerHeight * -5,
+    duration: 3,
+    scale: 3,
+    opacity: 0,
+  })
+  .to(".sculpture44", {
+    y: innerHeight * -5,
+    duration: 3,
+    scale: 3,
+    opacity: 0,
+  })
+  .to(".sculpture45", {
+    y: innerHeight * -5,
+    duration: 3,
+    scale: 3,
+    opacity: 0,
+  })
+  .to(".sculpture46", {
+    y: innerHeight * -5,
+    duration: 3,
+    scale: 3,
+    opacity: 0,
+  })
+  .to(".sculpture47", {
+    y: innerHeight * -5,
+    duration: 3,
+    scale: 3,
+    opacity: 0,
+  })
+  .to(".sculpture48", {
+    y: innerHeight * -5,
+    duration: 3,
+    scale: 3,
+    opacity: 0,
+  })
+  .to(".sculpture49", {
+    y: innerHeight * -5,
+    duration: 3,
+    scale: 3,
+    opacity: 0,
+  })
+  .from(".content-box-top h1", { x: -5, opacity: 0, duration: 5 })
+  .from(".content-box-bottom h3", { x: 5, opacity: 0, duration: 5 });
+
+gsap
+  .timeline({
+    scrollTrigger: {
+      trigger: "#article",
+      start: "top center",
+      end: "bottom top",
+      markers: false,
+      scrub: 5,
+      pin: true,
+    },
+  })
+  .from("#article", { opacity: 0 });
